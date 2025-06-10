433e6bf32f952f6365ff3d8edaadfc8868a4d90f
Alexey Navarkin <navarka.all@gmail.com>
2025-06-10 09:42:01 +0300

    Add config parsing.

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,9 @@ import (
 	"crypto/tls"
 	"log"
 	"net/http"
+	"os"
 
+	config "github.com/ThomasObenaus/go-conf"
 	"github.com/go-routeros/routeros/v3"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -12,18 +14,44 @@ import (
 	"github.com/alexeynavarkin/mikrotik-exporter/internal/collector"
 )
 
+type Config struct {
+	Targets []struct {
+		Address  string `cfg:"{'name':'address'}"`
+		Username string `cfg:"{'name':'username'}"`
+		Password string `cfg:"{'name':'password'}"`
+	} `cfg:"{'name':'targets'}"`
+}
+
 func main() {
+	cfg := Config{}
+
+	cfgProvider, err := config.NewConfigProvider(
+		&cfg,
+		"MIKROTIK_EXPORTER",
+		"MIKROTIK_EXPORTER",
+	)
+	if err != nil {
+		log.Fatal("failed to build config provider %w", err)
+	}
+
+	err = cfgProvider.ReadConfig(os.Args)
+	if err != nil {
+		log.Println("failed to load config", err)
+		log.Println(cfgProvider.Usage())
+		os.Exit(-1)
+	}
+
 	// Connect to MikroTik using v3 client
 	client, err := routeros.DialTLS(
-		"",
-		"",
-		"",
+		cfg.Targets[0].Address,
+		cfg.Targets[0].Username,
+		cfg.Targets[0].Password,
 		&tls.Config{
 			InsecureSkipVerify: true,
 		},
 	)
 	if err != nil {
-		log.Fatalf("Failed to connect to MikroTik: %v", err)
+		log.Fatalf("failed to connect to MikroTik: %v", err)
 	}
 	defer client.Close()
 
--- go.mod
+++ go.mod
@@ -8,12 +8,28 @@ require (
 )
 
 require (
+	github.com/ThomasObenaus/go-conf v0.1.3 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pelletier/go-toml v1.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.62.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/spf13/afero v1.1.2 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/jwalterweatherman v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.7.1 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
+	gopkg.in/ini.v1 v1.51.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
